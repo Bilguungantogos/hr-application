7a247f9cc4180c4301ae01224526e472566c26b7
Bilguun Gantogos <gantogos.bilguun48@gmail.com>
2024-04-20 09:35:42 +0800

    multer ustgav

--- hr-backapp/src/index.ts
+++ hr-backapp/src/index.ts
@@ -6,15 +6,7 @@ import { errorHandler } from "./middleWare/errorHandler";
 import auth from "./router/auth";
 import job from "./router/job";
 import jobApplication from "./router/jobApplication";
-import multer from "multer";
-import { Response } from "express";
-import { IReq } from "./utils/interface";
-import { authenticate } from "./middleWare/auth";
-import User from "./model/user";
-import fs from "fs";
-import AWS from "aws-sdk";
 
-const s3 = new AWS.S3();
 dotenv.config();
 
 const MONGO_URI = process.env.MONGO_URI as string;
@@ -30,44 +22,44 @@ app.use("/job", job);
 app.use("/application", jobApplication);
 app.use(errorHandler);
 
-AWS.config.update({
-  accessKeyId: process.env.ACCESS_KEY_ID,
-  secretAccessKey: process.env.ACCESS_SECRET_KEY,
-  region: "your_bucket_region",
-});
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./files");
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now();
-    cb(null, uniqueSuffix + file.originalname);
-  },
-});
-
-const upload = multer({ storage: storage });
-app.use("/files", express.static("files"));
-app.post(
-  "/upload-files",
-  upload.single("file"),
-  authenticate,
-  async (req: IReq, res: Response) => {
-    try {
-      console.log(req.file);
-      const uploadedCV = await User.updateOne(
-        { _id: req.user._id },
-        {
-          cv: req.file?.filename,
-        }
-      );
-      console.log(uploadedCV, "aaaaa");
-    } catch (error) {
-      console.log(error);
-    }
-  }
-);
-
 app.listen(8080, () => {
   console.log("server is running");
 });
+
+// AWS.config.update({
+//   accessKeyId: process.env.ACCESS_KEY_ID,
+//   secretAccessKey: process.env.ACCESS_SECRET_KEY,
+//   region: "your_bucket_region",
+// });
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./files");
+//   },
+//   filename: function (req, file, cb) {
+//     const uniqueSuffix = Date.now();
+//     cb(null, uniqueSuffix + file.originalname);
+//   },
+// });
+
+// const upload = multer({ storage: storage });
+// app.use("/files", express.static("files"));
+// app.post(
+//   "/upload-files",
+//   upload.single("file"),
+//   authenticate,
+//   async (req: IReq, res: Response) => {
+//     try {
+//       console.log(req.file);
+//       const uploadedCV = await User.updateOne(
+//         { _id: req.user._id },
+//         {
+//           cv: req.file?.filename,
+//         }
+//       );
+//       console.log(uploadedCV, "aaaaa");
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }
+// );
